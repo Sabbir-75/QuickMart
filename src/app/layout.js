347c2ca51730c81a12ec11dba28c19795cc6fc25
Sabbir-75 <mdsabbirhossain9200@gmail.comm>
2025-08-22 19:49:29 +0600

    Quickmart Apllication setup and added navbar done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./Components/Navbar/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,13 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div>
+          <Navbar></Navbar>
+          <div>
+            {children}
+          </div>
+
+        </div>
       </body>
     </html>
   );
